6c61fd0f5c357830f8f4a10d10739d9e7500f6f1
Fani Keorapetse <keorapetse.fani@dimensiondata.com>
2024-07-02 16:12:38 +0200

    Updated the code

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import VueVideoPlayer from '@videojs-player/vue'
-import 'video.js/dist/video-js.css'
 
 const app = createApp(App)
-app.use(VueVideoPlayer)
 app.mount('#app')
