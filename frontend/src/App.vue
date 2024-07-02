24730c2489d4b9ce1fea31d726ba3d4507fc5f20
Fani Keorapetse <keorapetse.fani@dimensiondata.com>
2024-07-02 14:36:18 +0200

    Updated the ui

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,46 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from "vue";
+// import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+// import { VideoPlayer } from "@videojs-player/vue";
+
+const message = ref("");
+
+onMounted(async () => {
+  const res = await axios.get("api/getDetails");
+  message.value = res.data;
+});
+
+// const option = { src: "rtsp://88.159.118.92:9000/live", type: "video/rtsp" };
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const video = document.getElementById("video");
+//   const hlsUrl = "http://127.0.0.1:8000/output.m3u8";
+
+//   if (Hls.isSupported()) {
+//     const hls = new Hls();
+//     hls.loadSource(hlsUrl);
+//     hls.attachMedia(video);
+//     hls.on(Hls.Events.MANIFEST_PARSED, function () {
+//       video.play();
+//     });
+//   } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+//     video.src = hlsUrl;
+//     video.addEventListener("loadedmetadata", function () {
+//       video.play();
+//     });
+//   } else {
+//     console.error("HLS is not supported in this browser");
+//   }
+// });
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div>Welcome {{ message }}</div>
+    <!-- <video-player :sources="[option]" controls :volume="0.6" autoplay /> -->
+    <!-- <video id="video" width="640" height="360" controls></video> -->
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
